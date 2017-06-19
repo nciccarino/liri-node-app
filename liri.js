b733c52405749e0724f937043e2b3e42e2095681
Nicholas A Ciccarino <nciccarino@gmail.com>
2017-06-19 18:03:01 -0400

    testing gitignore

--- liri.js
+++ liri.js
@@ -1,19 +1,42 @@
 var fs = require("fs"); 
 
-switch (command) {
-	case "my-tweets":
+var request = require("request"); 
 
+var action = process.argv[2]; 
+
+switch (action) {
+	case "my-tweets":
+		twitter();
 		break;
 
 	case "spotify-this-song":
-
+		spotify();
 		break;
 
 	case "movie-this":
-
+		omdb(); 
 		break; 
 
 	case "do-what-it-says":
-
+		doWhatItSays();
 		break; 
+}
+
+//node liri.js "my-tweets"
+
+function twitter() {
+	var fs = require("fs"); 
+	var keys = require("./keys.js"); 
+	//console.log(keys.twitterKeys); 
+	var Tweet = require("twitter"); 
+}
+
+//node liri.js "spotify-this-song" <"song name here">
+function spotify() {
+
+}
+
+//node liri.js "movie-this" <"movie name here">
+function omdb() {
+
 }
