4e628e5f7e1e5c47119c8e9d00776c5cbc455bd9
Nicholas A Ciccarino <nciccarino@gmail.com>
2017-06-18 21:15:52 -0400

    initial commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<s4HyWGPfsIGW5QnxDEjCBYLDz>',
+  consumer_secret: '<riXWXCSM6g3udCvkZzfIP2GNLpTKf2L2riLD4Dye3MmM0obrdX>',
+  access_token_key: '<876581687836237824-F3qRHCQUAXZj6XtN71GqNglapmNRzXd>',
+  access_token_secret: '<Uw4ap1RLTUm9NTHIvtPBVRfYbkxOaItXBEbU5kTkMyuiR>',
+}; 
---
+++ liri.js
@@ -0,0 +1,19 @@
+var fs = require("fs"); 
+
+switch (command) {
+	case "my-tweets":
+
+		break;
+
+	case "spotify-this-song":
+
+		break;
+
+	case "movie-this":
+
+		break; 
+
+	case "do-what-it-says":
+
+		break; 
+}
