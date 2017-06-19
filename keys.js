1ab8c89e6d4ba63ec31dd7ffddd6763082b07362
Nicholas A Ciccarino <nciccarino@gmail.com>
2017-06-19 19:38:06 -0400

    twitter working

--- keys.js
+++ keys.js
@@ -1,8 +1,9 @@
 console.log('this is loaded');
+console.log("-----------------------------");
 
 exports.twitterKeys = {
-  consumer_key: '<s4HyWGPfsIGW5QnxDEjCBYLDz>',
-  consumer_secret: '<riXWXCSM6g3udCvkZzfIP2GNLpTKf2L2riLD4Dye3MmM0obrdX>',
-  access_token_key: '<876581687836237824-F3qRHCQUAXZj6XtN71GqNglapmNRzXd>',
-  access_token_secret: '<Uw4ap1RLTUm9NTHIvtPBVRfYbkxOaItXBEbU5kTkMyuiR>',
+  consumer_key: 'Akyp7R7GkVQYg8Oa9I3JOOfI0',
+  consumer_secret: 'Tsc76DHpRZxMgz3XVdnYgOWYk9ezGCVpwFlqduAge61QSHdWnN',
+  access_token_key: '876581687836237824-F3qRHCQUAXZj6XtN71GqNglapmNRzXd',
+  access_token_secret: 'Uw4ap1RLTUm9NTHIvtPBVRfYbkxOaItXBEbU5kTkMyuiR'
 }; 
--- liri.js
+++ liri.js
@@ -1,7 +1,9 @@
+//dependencies
 var fs = require("fs"); 
-
 var request = require("request"); 
+var Twitter = require("twitter"); 
 
+//action
 var action = process.argv[2]; 
 
 switch (action) {
@@ -20,23 +22,57 @@ switch (action) {
 	case "do-what-it-says":
 		doWhatItSays();
 		break; 
-}
+};
+
+// //functon to log commands
+// function log() {
+// 	fs.appendFile(log.txt, "\n" + action + "\n", function(err) {
+// 		// If an error was experienced we say it.
+//     	if (err) {
+//     		console.log(err);
+//   		}
+
+//   		// If no error is experienced, we'll log the phrase "Content Added" to our node console.
+//   		else {
+//     		console.log("Content Added!");
+//   		}
+// 	})
+// }; 
+
 
 //node liri.js "my-tweets"
 
 function twitter() {
+	//log(); 
+
 	var fs = require("fs"); 
 	var keys = require("./keys.js"); 
-	//console.log(keys.twitterKeys); 
-	var Tweet = require("twitter"); 
-}
+	
+	var client = new Twitter(keys.twitterKeys); 
+
+	// client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
+	//    console.log(tweets);
+	// });
+
+	var params = {screen_name: "nciccarino"};
+	client.get("statuses/user_timeline", params, function(error, tweets, responses) {
+		if(!error) {
+			for(var i = 0; i < tweets.length; i++) {
+				console.log(tweets[i].text); 
+				console.log(tweets[i].created_at); 
+				console.log("-----------------------------");
+			}
+		}
+	})
+	
+};
 
 //node liri.js "spotify-this-song" <"song name here">
 function spotify() {
 
-}
+};
 
 //node liri.js "movie-this" <"movie name here">
 function omdb() {
 
-}
+};
